4fc53e7087f2488d44f7c40bd80a2d59be553abd
Simon Malpel (SimonOriginal) <simon.malpel@orange.fr>
2019-08-25 23:52:21 +0200

    Display errors

--- public/index.php
+++ public/index.php
@@ -1,5 +1,9 @@
 <?php
 
+    ini_set('display_errors', 1);
+    ini_set('display_startup_errors', 1);
+    error_reporting(E_ALL);
+
     $path = (realpath(__DIR__ . '/..') . '/src');
     require $path.'/autoload.php';
 
